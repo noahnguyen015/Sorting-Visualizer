d0c2df205702b9b5400fc0553077e2ca405fe5ad
Noah Nguyen <noahn1105@MSI.localdomain>
2024-07-10 21:44:33 -0700

    Comments and Merge Sort File

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ const insertionSort = document.getElementById("insertionSort");
 changeButton.addEventListener("click", changeArr);
 resetButton.addEventListener("click", resetArr);
 
+//based on button pressed, used specified sorting method
 bubbleSort.addEventListener("click", ()=> sortArr("bubble"));
 insertionSort.addEventListener("click", () => sortArr("insertion"));
 
-
+//change the numbers of the array
 function changeArr(){
 
     for(let i = 0; i < n; i++){
+        //equation returns number from 5-100
         arr[i] = Math.floor(Math.random()*(100-5+1)+5);
     }
     org = [...arr];
@@ -29,6 +31,7 @@ function changeArr(){
 }
 
 function resetArr(){
+    //resets to pre-sorted array version of current array
     for(let i = 0; i < n; i++){
         arr[i] = org[i];
     }
@@ -38,15 +41,19 @@ function resetArr(){
 
 function showArr(move){
 
+    //clear element to show updated chart
     chart.innerHTML = "";
 
     for(let i = 0; i < arr.length; i++){
 
+        //create a bar with specified height and add style
         const bar = document.createElement("div");
         bar.style.height = arr[i] + "%";
         bar.classList.add("bar");
         chart.appendChild(bar);
 
+        //if a move argument is specified, change the bar to certain color
+        //only on the array currently being looked at
         if(move && move.indices.includes(i))
         {
             const color = "rgb(81, 194, 81)";
@@ -57,6 +64,7 @@ function showArr(move){
 
 function sortArr(type){
 
+    //use a copy of the array
     const copy = [...arr];
     let movelist = [];
 
@@ -72,6 +80,8 @@ function sortArr(type){
 }
 
 function animateArr(movelist){
+    //animates the sorting of the array
+    //if length is 0 show the final sorted array
     if(movelist.length == 0)
         return showArr();
 
@@ -84,42 +94,56 @@ function animateArr(movelist){
 
     [i,j] = move.indices;
     
+    //swap the actual array
     if(move.type == "swap"){
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 
+    //sound playing to show array bars being swapped
+    //interpolation equation, in between 100-430Hz
     playSound(100+(arr[i]/100)*330);
     playSound(100+(arr[j]/100)*330);
     showArr(move);
 
+    //call the function every 50 seconds till there are no more moves
     setTimeout(() => animateArr(movelist), 50);
 }
 
 let audiocontext = null;
 
 function playSound(freq){
+    //create audiocontext object
     if(audiocontext == null)
         audiocontext = new AudioContext();
 
+    //how long the sound last
     const duration = 0.1;
 
     const osc = audiocontext.createOscillator();
 
+    //frequency value argument based on array value/bar height
     osc.frequency.value = freq;
 
     osc.start();
+    //stop after duration
     osc.stop(audiocontext.currentTime+duration);
 
     const node = audiocontext.createGain();
-
+    
+    //control volume to be about 10%
     node.gain.value = 0.1;
 
+    //remove static in the sound
     node.gain.linearRampToValueAtTime(0,audiocontext.currentTime+duration);
 
+
+    //connect  oscillator --> node --> speakers
     osc.connect(node);
+    //destination is usually speakers/headphones
     node.connect(audiocontext.destination);   
 }
 
+//call to show an inital array when loaded in
 changeArr();
 showArr();
 
--- sorting.js
+++ sorting.js
@@ -1,8 +1,13 @@
 
-export function BubbleSort(arr){
+//the goal of BubbleSort is to move the greates number to the top by "bubbling" it
+//this is done by one by one comparisons till the top is reached
 
+export function BubbleSort(arr){ 
+
+    //move list is used to store the moves of the swapping
     let movelist = [];
 
+    //checks if the array needs to be checking for swaps
     let isSwapped = false;
 
     for(let i = 0; i < arr.length; i++){
@@ -10,37 +15,51 @@ export function BubbleSort(arr){
         isSwapped = false;
 
         for(let j = 1; j < arr.length-i; j++){
-
+            //comparsion, to see which bars are to be compared
             movelist.push({indices: [j-1, j], type: "compare"});
 
             if(arr[j-1] > arr[j]){
+                //previous element is greater than current? swap them
+                //if there is one push to the movelist and swap them
                 movelist.push({indices: [j-1, j], type: "swap"});
                 [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
-                
+                //means there was a swap, so keep going
                 isSwapped = true;
             }
         }
 
+    
+        //isSwapped = false; means all elements are sorted
         if(!isSwapped)
             break;
     }
     return movelist;
 }
 
+//Insertion Sort's goal is to created a pre-sorted array on the left
+//Then to use the presorted array and place the next element on right in the proper place
+
 export function InsertionSort(arr){
 
+    //store each move for animation
     let movelist = [];
 
     for(let i = 0; i < arr.length; i++){
-
+        //goes form left to right
         for(let j = i-1; j >= 0; j--){
-
+            
+            //look at the element before the ith index and what is in front of it
             movelist.push({indices: [j, j+1], type: "compare"});
 
             if(arr[j] > arr[j+1]){
+                //if the one before is bigger, shift it to the right
+                //meaning j --> to j+1
+                //and j+1 value --> j
                 movelist.push({indices: [j, j+1], type: "swap"});
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
             }else{
+                //if the value before (j) is not bigger than j+1
+                // it is in the correct spot (j+1)
                 break;
             }
         }
